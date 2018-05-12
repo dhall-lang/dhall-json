915e6d4606e3c34300f6bee463ee603b63902f21
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-05-12 14:01:42 -0700

    Switch to `optparse-applicative`
    This switches to using the lower-level `optparse-applicative` for
    command-line option parsing.  The main motivation for this is to support
    more complex command-line option parsing that may be necessary for
    https://github.com/dhall-lang/dhall-json/issues/27

--- dhall-to-yaml/Main.hs
+++ dhall-to-yaml/Main.hs
@@ -1,14 +1,11 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TypeOperators     #-}
 
 module Main where
 
 import Control.Exception (SomeException)
-import Options.Generic (Generic, ParseRecord, Wrapped, type (<?>), type (:::))
+import Data.Monoid ((<>))
+import Options.Applicative (Parser, ParserInfo)
 
 import qualified Control.Exception
 import qualified Data.ByteString
@@ -17,22 +14,46 @@ import qualified Data.Yaml
 import qualified Dhall
 import qualified Dhall.JSON
 import qualified GHC.IO.Encoding
-import qualified Options.Generic
+import qualified Options.Applicative
 import qualified System.Exit
 import qualified System.IO
 
-data Options w = Options
-    { explain  :: w ::: Bool <?> "Explain error messages in detail"
-    , omitNull :: w ::: Bool <?> "Omit record fields that are null"
-    } deriving (Generic)
+data Options = Options
+    { explain  :: Bool
+    , omitNull :: Bool
+    }
 
-instance ParseRecord (Options Wrapped)
+parseOptions :: Parser Options
+parseOptions = Options.Applicative.helper <*> do
+    explain  <- parseExplain
+    omitNull <- parseOmitNull
+    return (Options {..})
+  where
+    parseExplain =
+        Options.Applicative.switch
+            (   Options.Applicative.long "explain"
+            <>  Options.Applicative.help "Explain error messages in detail"
+            )
+
+    parseOmitNull =
+        Options.Applicative.switch
+            (   Options.Applicative.long "omitNull"
+            <>  Options.Applicative.help "Omit record fields that are null"
+            )
+
+parserInfo :: ParserInfo Options
+parserInfo =
+    Options.Applicative.info
+        parseOptions
+        (   Options.Applicative.fullDesc
+        <>  Options.Applicative.progDesc "Compile Dhall to YAML"
+        )
 
 main :: IO ()
 main = do
     GHC.IO.Encoding.setLocaleEncoding GHC.IO.Encoding.utf8
 
-    Options{..} <- Options.Generic.unwrapRecord "Compile Dhall to JSON"
+    Options {..} <- Options.Applicative.execParser parserInfo
 
     handle $ do
         let explaining = if explain then Dhall.detailed else id
