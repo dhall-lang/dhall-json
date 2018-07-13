f44665366b95dbc3ecd33d8c927867c078438952
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-07-12 20:59:03 -0700

    Refactor Nix code (#42)
    This uses the `readDirectory` utility for ease of testing updates to
    dependencies

---
+++ default.nix
@@ -0,0 +1,37 @@
+let
+  fetchNixpkgs = import ./nix/fetchNixpkgs.nix;
+
+  nixpkgs = fetchNixpkgs {
+    rev = "804060ff9a79ceb0925fe9ef79ddbf564a225d47";
+
+    sha256 = "01pb6p07xawi60kshsxxq1bzn8a0y4s5jjqvhkwps4f5xjmmwav3";
+
+    outputSha256 = "0ga345hgw6v2kzyhvf5kw96hf60mx5pbd9c4qj5q4nan4lr7nkxn";
+  };
+
+  readDirectory = import ./nix/readDirectory.nix;
+
+  config = {
+    packageOverrides = pkgs: {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides =
+          let
+            manualOverrides = haskellPackagesNew: haskellPackagesOld: {
+              prettyprinter =
+                pkgs.haskell.lib.dontCheck haskellPackagesOld.prettyprinter;
+            };
+
+          in
+            pkgs.lib.composeExtensions (readDirectory ./nix) manualOverrides;
+      };
+    };
+  };
+
+  pkgs =
+    import nixpkgs { inherit config; };
+
+in
+  { inherit (pkgs.haskellPackages) dhall-json;
+
+    shell = (pkgs.haskell.lib.doBenchmark pkgs.haskellPackages.dhall-json).env;
+  }
