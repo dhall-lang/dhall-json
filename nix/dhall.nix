9a9e842798f203a576f17b6d01303b0baca0ce82
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-10-11 19:22:44 -0700

    Fix the Nix build
    This also includes some cosmetic changes from my own branch

--- nix/dhall.nix
+++ nix/dhall.nix
@@ -1,39 +1,36 @@
 { mkDerivation, ansi-terminal, base, bytestring, case-insensitive
 , cborg, containers, contravariant, criterion, cryptonite, deepseq
 , Diff, directory, doctest, exceptions, fetchgit, filepath
-, hashable, haskeline, http-client, http-client-tls
-, insert-ordered-containers, lens-family-core, megaparsec, memory
-, mockery, mtl, optparse-applicative, parsers, prettyprinter
-, prettyprinter-ansi-terminal, QuickCheck, quickcheck-instances
-, repline, scientific, serialise, stdenv, tasty, tasty-hunit
-, tasty-quickcheck, template-haskell, text, transformers
-, unordered-containers, vector
+, haskeline, http-client, http-client-tls, lens-family-core
+, megaparsec, memory, mockery, mtl, optparse-applicative, parsers
+, prettyprinter, prettyprinter-ansi-terminal, QuickCheck
+, quickcheck-instances, repline, scientific, serialise, stdenv
+, tasty, tasty-hunit, tasty-quickcheck, template-haskell, text
+, transformers, unordered-containers, vector
 }:
 mkDerivation {
   pname = "dhall";
   version = "1.17.0";
   src = fetchgit {
     url = "https://github.com/dhall-lang/dhall-haskell.git";
-    sha256 = "0fj61llfr2sz8lh2xpryzp7q0ahgqbzgf72ppw34w5ibq5a6gd9l";
-    rev = "fedfa8e41ea8da32c2ee779ce65982544ed4800f";
+    sha256 = "0pivzmmx7qlkqkan0xzjm0il114lxhvpwwlvnc6fdfjaawvgb5bg";
+    rev = "afffa17be44047169cfb335a65ebb63d9d7a7868";
   };
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     ansi-terminal base bytestring case-insensitive cborg containers
     contravariant cryptonite Diff directory exceptions filepath
-    hashable haskeline http-client http-client-tls
-    insert-ordered-containers lens-family-core megaparsec memory mtl
-    optparse-applicative parsers prettyprinter
+    haskeline http-client http-client-tls lens-family-core megaparsec
+    memory mtl optparse-applicative parsers prettyprinter
     prettyprinter-ansi-terminal repline scientific serialise
     template-haskell text transformers unordered-containers vector
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
-    base containers deepseq directory doctest filepath hashable
-    insert-ordered-containers mockery prettyprinter QuickCheck
-    quickcheck-instances serialise tasty tasty-hunit tasty-quickcheck
-    text transformers vector
+    base containers deepseq directory doctest filepath mockery
+    prettyprinter QuickCheck quickcheck-instances serialise tasty
+    tasty-hunit tasty-quickcheck text transformers vector
   ];
   benchmarkHaskellDepends = [
     base bytestring containers criterion directory serialise text
